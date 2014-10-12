bd1ac3030042667b690f6dbb4913be29387fec3e
Kinder <st0912@gmail.com>
2014-10-12 22:38:07 +0800

    Add normal boundary value test

--- HW1/triangle_unittest.cc
+++ HW1/triangle_unittest.cc
@@ -1,6 +1,25 @@
 #include "Triangle.h"
 #include "gtest/gtest.h"
 
+TEST(BoundaryValueTest, Normal)
+{
+    EXPECT_STREQ("Isosceles", checkTriangleType(100, 100, 1));
+    EXPECT_STREQ("Isosceles", checkTriangleType(100, 100, 2));
+    EXPECT_STREQ("Equilateral", checkTriangleType(100, 100, 100));
+    EXPECT_STREQ("Isosceles", checkTriangleType(100, 100, 199));
+    EXPECT_STREQ("Not a triangle", checkTriangleType(100, 100, 200));
+    EXPECT_STREQ("Isosceles", checkTriangleType(100, 1, 100));
+    EXPECT_STREQ("Isosceles", checkTriangleType(100, 2, 100));
+    EXPECT_STREQ("Equilateral", checkTriangleType(100, 100, 100));
+    EXPECT_STREQ("Isosceles", checkTriangleType(100, 199, 100));
+    EXPECT_STREQ("Not a triangle", checkTriangleType(100, 200, 100));
+    EXPECT_STREQ("Isosceles", checkTriangleType(1, 100, 100));
+    EXPECT_STREQ("Isosceles", checkTriangleType(2, 100, 100));
+    EXPECT_STREQ("Equilateral", checkTriangleType(100, 100, 100));
+    EXPECT_STREQ("Isosceles", checkTriangleType(199, 100, 100));
+    EXPECT_STREQ("Not a triangle", checkTriangleType(200, 100, 100));
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
