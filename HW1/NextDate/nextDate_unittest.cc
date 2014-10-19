92db16f7c4825050c82f0925e262a1383cb1f7a1
Kinder <st0912@gmail.com>
2014-10-19 09:32:09 +0800

    Add equivalence class test

--- HW1/NextDate/nextDate_unittest.cc
+++ HW1/NextDate/nextDate_unittest.cc
@@ -23,7 +23,45 @@ TEST(BoundaryValueTest, Robust)
     EXPECT_EQ("Invalid input", nextDate(13, 1, 2000));
 }
 
+TEST(EquivalenceClassTest, WeakNormal)
+{
+    EXPECT_EQ("6/15/2000", nextDate(6, 14, 2000));
+    EXPECT_EQ("7/30/1996", nextDate(7, 29, 1996));
+    EXPECT_EQ("Impossible date", nextDate(2, 30, 2002));
+    EXPECT_EQ("Impossible date", nextDate(6, 31, 2000));
+}
 
+TEST(EquivalenceClassTest, WeakRobust)
+{
+    EXPECT_EQ("Invalid input", nextDate(1, 1, 1811));
+    EXPECT_EQ("Invalid input", nextDate(1, 32, 1815));
+    EXPECT_EQ("Invalid input", nextDate(13, 1, 2000));
+    EXPECT_EQ("Invalid input", nextDate(0, 1, 2012));
+}
+
+TEST(EquivalenceClassTest, StrongNormal)
+{
+    EXPECT_EQ("6/15/2000", nextDate(6, 14, 2000));
+    EXPECT_EQ("6/15/1996", nextDate(6, 14, 1996));
+    EXPECT_EQ("6/15/2002", nextDate(6, 14, 2002));
+    EXPECT_EQ("6/30/2000", nextDate(6, 29, 2000));
+    EXPECT_EQ("6/30/1996", nextDate(6, 29, 1996));
+    EXPECT_EQ("6/30/2002", nextDate(6, 29, 2002));
+    EXPECT_EQ("7/1/2000", nextDate(6, 30, 2000));
+    EXPECT_EQ("7/1/1996", nextDate(6, 30, 1996));
+    EXPECT_EQ("7/1/2002", nextDate(6, 30, 2002));
+    EXPECT_EQ("Impossible date", nextDate(6, 31, 2000));
+    EXPECT_EQ("Impossible date", nextDate(6, 31, 1996));
+    EXPECT_EQ("Impossible date", nextDate(6, 31, 2002));
+}
+
+TEST(EquivalenceClassTest, StrongRobust)
+{
+    EXPECT_EQ("Invalid input", nextDate(1, 0, 1811));
+    EXPECT_EQ("Invalid input", nextDate(12, 32, 2013));
+    EXPECT_EQ("Invalid input", nextDate(0, 1, 1818));
+    EXPECT_EQ("Invalid input", nextDate(1, -1, 1999));
+}
 
 int main(int argc, char **argv)
 {
