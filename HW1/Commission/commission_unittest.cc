24518a04303660a6f73c068e8a47db9e2d501fe5
Kinder <st0912@gmail.com>
2014-10-21 01:29:03 +0800

    Add equivalence class test

--- HW1/Commission/commission_unittest.cc
+++ HW1/Commission/commission_unittest.cc
@@ -21,6 +21,40 @@ TEST(BoundaryValueTest, Robust)
     EXPECT_EQ(-1, calculateCommission(48, 48, 91));
 }
 
+TEST(EquivalenceClassTest, WeakNormal)
+{
+    EXPECT_EQ(13, calculateCommission(1, 2, 1));
+    EXPECT_EQ(97.5, calculateCommission(10, 10, 9));
+    EXPECT_EQ(215.5, calculateCommission(18, 17, 18));
+    EXPECT_EQ(1414, calculateCommission(70, 79, 90));
+}
+
+TEST(EquivalenceClassTest, WeakRobust)
+{
+    EXPECT_EQ(-1, calculateCommission(-2, 40, 45));
+    EXPECT_EQ(-1, calculateCommission(71, 40, 45));
+    EXPECT_EQ(-1, calculateCommission(35, -1, 45));
+    EXPECT_EQ(-1, calculateCommission(35, 81, 45));
+    EXPECT_EQ(-1, calculateCommission(35, 40, -1));
+    EXPECT_EQ(-1, calculateCommission(35, 40, 91));
+}
+
+TEST(EquivalenceClassTest, StrongNormal)
+{
+    EXPECT_EQ(50, calculateCommission(5, 5, 5));
+    EXPECT_EQ(106.75, calculateCommission(11, 10, 10));
+    EXPECT_EQ(226, calculateCommission(18, 19, 18));
+    EXPECT_EQ(1411, calculateCommission(69, 80, 90));
+}
+
+TEST(EquivalenceClassTest, StrongRobust)
+{
+    EXPECT_EQ(-1, calculateCommission(-2, -1, 45));
+    EXPECT_EQ(-1, calculateCommission(-2, 45, -1));
+    EXPECT_EQ(-1, calculateCommission(35, -1, -1));
+    EXPECT_EQ(-1, calculateCommission(-2, -1, -1));
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
