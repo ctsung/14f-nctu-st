9561aab9e35af8c029a2b94de41dcf6ce1c380ae
Kinder <st0912@gmail.com>
2014-10-21 01:05:52 +0800

    Add boundary value test

--- HW1/Commission/commission_unittest.cc
+++ HW1/Commission/commission_unittest.cc
@@ -1,6 +1,26 @@
 #include "commission.h"
 #include "gtest/gtest.h"
 
+TEST(BoundaryValueTest, Normal)
+{
+    EXPECT_EQ(10, calculateCommission(1, 1, 1));
+    EXPECT_EQ(12.5, calculateCommission(1, 1, 2));
+    EXPECT_EQ(220, calculateCommission(18, 18, 18));
+    EXPECT_EQ(820, calculateCommission(48, 48, 48));
+    EXPECT_EQ(1411, calculateCommission(69, 80, 90));
+    EXPECT_EQ(1420, calculateCommission(70, 80, 90));
+}
+
+TEST(BoundaryValueTest, Robust)
+{
+    EXPECT_EQ(-1, calculateCommission(0, 48, 48));
+    EXPECT_EQ(-1, calculateCommission(48, 0, 48));
+    EXPECT_EQ(-1, calculateCommission(48, 48, 0));
+    EXPECT_EQ(-1, calculateCommission(71, 48, 48));
+    EXPECT_EQ(-1, calculateCommission(48, 81, 48));
+    EXPECT_EQ(-1, calculateCommission(48, 48, 91));
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
