d654b47cc52746bdc90203fd9f5d0fa4c20bf87b
Kinder <st0912@gmail.com>
2014-11-20 20:24:56 +0800

    Add  C1 test

--- HW2/Triangle/triangle_unittest.cc
+++ HW2/Triangle/triangle_unittest.cc
@@ -10,6 +10,23 @@ TEST(PathTest, C0)
     EXPECT_EQ("Invalid input", checkTriangleType(100, 202, 100));
 }
 
+TEST(PathTest, C1)
+{
+    EXPECT_EQ("Equilateral", checkTriangleType(5, 5, 5));
+    EXPECT_EQ("Isosceles", checkTriangleType(2, 2, 3));
+    EXPECT_EQ("Isosceles", checkTriangleType(2, 3, 2));
+    EXPECT_EQ("Scalene", checkTriangleType(3, 4, 5));
+    EXPECT_EQ("Not a triangle", checkTriangleType(4, 1, 2));
+    EXPECT_EQ("Not a triangle", checkTriangleType(1, 4, 2));
+    EXPECT_EQ("Not a triangle", checkTriangleType(2, 1, 4));
+    EXPECT_EQ("Invalid input", checkTriangleType(0, 100, 100));
+    EXPECT_EQ("Invalid input", checkTriangleType(100, 0, 100));
+    EXPECT_EQ("Invalid input", checkTriangleType(100, 100, 0));
+    EXPECT_EQ("Invalid input", checkTriangleType(201, 100, 100));
+    EXPECT_EQ("Invalid input", checkTriangleType(100, 201, 100));
+    EXPECT_EQ("Invalid input", checkTriangleType(100, 100, 201));
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
